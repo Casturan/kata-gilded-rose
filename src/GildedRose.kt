52264151ec0e15c291180f2bc35e9d91cab368cd
Deniz Turan <deniz.turan@philips.com>
2018-11-22 17:40:10 +0100

    All green

--- src/GildedRose.kt
+++ src/GildedRose.kt
@@ -7,15 +7,17 @@ class GildedRose(var items: Array<Item>) {
   private val agedBrie = "Aged Brie"
 
   fun updateQuality() {
-    for (i in items.indices) {
-      val item = items[i]
+    for (item in items) {
       if (item.name != agedBrie && item.name != backstagePass) {
+
         if (item.quality > 0) {
           if (item.name != sulfuras) {
             item.quality = item.quality - 1
           }
         }
+
       } else {
+
         if (item.quality < 50) {
           item.quality = item.quality + 1
 
@@ -33,11 +35,17 @@ class GildedRose(var items: Array<Item>) {
             }
           }
         }
+
       }
+
+
 
       if (item.name != sulfuras) {
         item.sellIn = item.sellIn - 1
       }
+
+
+
 
       if (item.sellIn < 0) {
         if (item.name != agedBrie) {
